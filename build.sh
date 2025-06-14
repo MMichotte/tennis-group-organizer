2e6788b82931b8d4b5043b42ea040fdcec75369b
MMichotte <martin.michotte@gmail.com>
2025-06-14 16:09:43 +0200

    refactor(docker): update build script

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 
+platform=linux/amd64
 img_name="tennis-organizer-app"
 tag="latest"
 dockerhub_username="mmichotte"
 
 dockerhub_img=$dockerhub_username/$img_name:$tag
 
-docker build -t $img_name . &&
+docker build --platform $platform -t $img_name . &&
   docker tag $img_name $dockerhub_img &&
   docker login &&
   docker push $dockerhub_img
